488b72755ab33d0a7b08b79d284e619772b8af0d
jinwoo.baek <jinwoo.baek@linecorp.com>
2025-10-19 20:05:42 +0900

    add card Hand Drawer init deck

--- Assets/Undead Survivor/Codes/CardHandDrawer.cs
+++ Assets/Undead Survivor/Codes/CardHandDrawer.cs
@@ -1,16 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class CardHandDrawer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Deck Sprites")]
+    [Tooltip("인스펙터에 카드 54장을 넣거나 비워두면 Resources/Cards/에서 자동 로드")]
+    public List<Sprite> deck = new List<Sprite>();
 
-    // Update is called once per frame
-    void Update()
+    [Header("UI Targets (5)")]
+    public Image[] slots;
+
+    void Awake()
     {
-        
     }
 }
